854a60661fd2088136d644996aceed0121f1b5f0
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-07-01 19:04:40 +0100

    add index page

---
+++ client/submit.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(input.value);
+  input.value = '';
+});
