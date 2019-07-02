a7f80417ad964547aace705b345d543356ccb60f
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-07-02 10:07:53 +0100

    server responds with pageinfo object

--- client/submit.js
+++ client/submit.js
@@ -3,6 +3,17 @@ const input = document.querySelector('#input');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  console.log(input.value);
+  fetch('/', {
+    method: 'POST',
+    headers: new Headers({ 'content-type': 'application/json' }),
+    body: JSON.stringify({ url: input.value })
+  })
+    .then(response => {
+      return response.text();
+    })
+    .then(text => {
+      const result = JSON.parse(text);
+      console.log(result);
+    });
   input.value = '';
 });
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
+const getPageInfo = require('./getPageInfo');
 const PORT = 9090;
 
 const app = express();
 
 app.use(express.static(__dirname + '/client'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname) + '/client/index.html');
+});
+
+app.post('/', (req, res) => {
+  const { url } = req.body;
+  getPageInfo(url)
+    .then(pageInfo => {
+      res.status(200).send(pageInfo);
+    })
+    .catch(err => res.send(err));
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
